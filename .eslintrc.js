2811f63ee2a142b05eda6e5ee72a60dc094b71cf
wangxingkang <wang_xingkang@qq.com>
2017-07-25 15:50:22 +0800

    添加代码审查

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "root": true,
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  // 定义全局变量
+  "globals": {
+    "jQuery": true,
+    "$": true,
+    "avalon": true
+  },
+  "rules": {
+    "no-cond-assign": [2, "always"],
+    "no-constant-condition": 2,
+    "no-debugger": 2,
+    "no-dupe-args": 2,
+    "no-dupe-keys": 2,
+    "no-duplicate-case": 2,
+    "no-func-assign": 2,
+    "no-obj-calls": 2,
+    "no-regex-spaces": 2,
+    "no-sparse-arrays": 2,
+    "no-unexpected-multiline": 2,
+    "no-unreachable": 2,
+    "use-isnan": 2,
+    "valid-typeof": 2,
+    "eqeqeq": [0, "always"],
+    "quotes": [2, "single"],
+    "no-caller": 2,
+    "no-eval": 2,
+    "no-redeclare": 2,
+    "no-undef": 2,
+    "no-unused-vars": 0,
+    "no-use-before-define": 2,
+    "comma-dangle": [1, "never"],
+    "no-const-assign": 2,
+    "no-dupe-class-members": 2
+  }
+};
